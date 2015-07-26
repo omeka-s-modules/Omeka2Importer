b456af5f31fa6a8daf1ed35ebdf1faf00295988c
patrickmj <patrickmjchnm@gmail.com>
2015-07-26 14:04:37 -0400

    update install to new db structure

--- Module.php
+++ Module.php
@@ -19,7 +19,7 @@ class Module extends AbstractModule
     {
         $connection = $serviceLocator->get('Omeka\Connection');
 
-        $connection->exec("CREATE TABLE omeka2item (id INT AUTO_INCREMENT NOT NULL, job_id INT NOT NULL, item_id INT NOT NULL, remote_item_id INT NOT NULL, uri VARCHAR(255) NOT NULL, last_modified DATETIME NOT NULL, INDEX IDX_59E62AA3BE04EA9 (job_id), UNIQUE INDEX UNIQ_59E62AA3126F525E (item_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB;");
+        $connection->exec("CREATE TABLE omeka2item (id INT AUTO_INCREMENT NOT NULL, job_id INT NOT NULL, item_id INT NOT NULL, remote_id INT NOT NULL, endpoint VARCHAR(255) NOT NULL, last_modified DATETIME NOT NULL, INDEX IDX_59E62AA3BE04EA9 (job_id), UNIQUE INDEX UNIQ_59E62AA3126F525E (item_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB;");
         $connection->exec("ALTER TABLE omeka2item ADD CONSTRAINT FK_59E62AA3BE04EA9 FOREIGN KEY (job_id) REFERENCES job (id);");
         $connection->exec("ALTER TABLE omeka2item ADD CONSTRAINT FK_59E62AA3126F525E FOREIGN KEY (item_id) REFERENCES item (id) ON DELETE CASCADE;");
 //        $connection->exec("");
