466ae8ca285ca5ebd3c5a0da2db222266268ae50
patrickmj <patrickmjchnm@gmail.com>
2016-09-19 12:00:52 -0400

    finish fixing bad merge

--- src/Service/Controller/IndexControllerFactory.php
+++ src/Service/Controller/IndexControllerFactory.php
@@ -8,19 +8,10 @@ use Interop\Container\ContainerInterface;
 
 class IndexControllerFactory implements FactoryInterface
 {
-<<<<<<< HEAD
-    public function createService(ServiceLocatorInterface $serviceLocator)
-    {
-        $serviceLocator = $serviceLocator->getServiceLocator();
-        $client = $serviceLocator->get('Omeka2Importer\Omeka2Client');
-        $indexController = new IndexController($client);
-
-=======
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
-    {   
+    {
         $client = $container->get('Omeka2Importer\Omeka2Client');
         $indexController = new IndexController($client);
->>>>>>> master
         return $indexController;
     }
 }
