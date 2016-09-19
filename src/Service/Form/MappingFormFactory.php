63f6b0e177ee2041e76066f620ed145717d725c2
Julian Maurice <julian.maurice@biblibre.com>
2016-09-19 10:33:18 +0200

    Fix to work with current S

--- src/Service/Form/MappingFormFactory.php
+++ src/Service/Form/MappingFormFactory.php
@@ -3,18 +3,15 @@
 namespace Omeka2Importer\Service\Form;
 
 use Omeka2Importer\Form\MappingForm;
-use Zend\ServiceManager\FactoryInterface;
-use Zend\ServiceManager\ServiceLocatorInterface;
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\Factory\FactoryInterface;
 
 class MappingFormFactory implements FactoryInterface
 {
-    protected $options = [];
-
-    public function createService(ServiceLocatorInterface $elements)
+    public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
     {
-        $form = new MappingForm(null, $this->options);
-        $serviceLocator = $elements->getServiceLocator();
-        $identity = $serviceLocator->get('Omeka\AuthenticationService')->getIdentity();
+        $form = new MappingForm(null, $options);
+        $identity = $services->get('Omeka\AuthenticationService')->getIdentity();
         $form->setOwner($identity);
 
         return $form;
