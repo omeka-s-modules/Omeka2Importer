ebd7695feee81abbf652fe32425e016ada74cd81
patrickmj <patrickmjchnm@gmail.com>
2016-11-11 12:49:11 -0500

    upgrade for deleting item sets w/ cascade

--- Module.php
+++ Module.php
@@ -20,6 +20,7 @@ class Module extends AbstractModule
         $connection->exec('ALTER TABLE omekaimport_record ADD CONSTRAINT FK_3185E9B1BE04EA9 FOREIGN KEY (job_id) REFERENCES job (id);');
         $connection->exec('ALTER TABLE omekaimport_record ADD CONSTRAINT FK_3185E9B1126F525E FOREIGN KEY (item_id) REFERENCES item (id) ON DELETE CASCADE;');
         $connection->exec('ALTER TABLE omekaimport_record ADD CONSTRAINT FK_3185E9B1960278D7 FOREIGN KEY (item_set_id) REFERENCES item_set (id);');
+        $connection->exec("ALTER TABLE omekaimport_record ADD CONSTRAINT FK_3185E9B1960278D7 FOREIGN KEY (item_set_id) REFERENCES item_set (id) ON DELETE CASCADE;");
 
         $connection->exec('CREATE TABLE omekaimport_import (id INT AUTO_INCREMENT NOT NULL, job_id INT NOT NULL, undo_job_id INT DEFAULT NULL, added_count INT NOT NULL, updated_count INT NOT NULL, comment VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_37FFB83DBE04EA9 (job_id), UNIQUE INDEX UNIQ_37FFB83D4C276F75 (undo_job_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB;');
         $connection->exec('ALTER TABLE omekaimport_import ADD CONSTRAINT FK_37FFB83DBE04EA9 FOREIGN KEY (job_id) REFERENCES job (id);');
@@ -37,5 +38,23 @@ class Module extends AbstractModule
         $connection->exec('ALTER TABLE omekaimport_import DROP FOREIGN KEY FK_37FFB83DBE04EA9');
         $connection->exec('ALTER TABLE omekaimport_import DROP FOREIGN KEY FK_37FFB83D4C276F75');
         $connection->exec('DROP TABLE omekaimport_import');
+    }
+    
+    public function upgrade($oldVersion, $newVersion,
+        ServiceLocatorInterface $serviceLocator
+    )
+    {
+        if (version_compare($oldVersion, '1.0.0-beta', '<')) {
+            $connection = $serviceLocator->get('Omeka\Connection');
+            $connection->exec("ALTER TABLE omekaimport_record DROP INDEX FK_3185E9B1960278D7, ADD UNIQUE INDEX UNIQ_3185E9B1960278D7 (item_set_id);");
+            $connection->exec("ALTER TABLE omekaimport_record DROP FOREIGN KEY FK_3185E9B1960278D7;");
+            $connection->exec("ALTER TABLE omekaimport_record ADD CONSTRAINT FK_3185E9B1960278D7 FOREIGN KEY (item_set_id) REFERENCES item_set (id) ON DELETE CASCADE;");
+        }
+
+        $sql = "
+        ALTER TABLE omekaimport_record DROP INDEX FK_3185E9B1960278D7, ADD UNIQUE INDEX UNIQ_3185E9B1960278D7 (item_set_id);
+        ALTER TABLE omekaimport_record DROP FOREIGN KEY FK_3185E9B1960278D7;
+        ALTER TABLE omekaimport_record ADD CONSTRAINT FK_3185E9B1960278D7 FOREIGN KEY (item_set_id) REFERENCES item_set (id) ON DELETE CASCADE;
+        ";
     }
 }
