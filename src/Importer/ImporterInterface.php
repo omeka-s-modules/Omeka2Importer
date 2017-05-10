d5909c8ce0bc5b076fcdccfd02669681814a314b
patrickmj <patrickmjchnm@gmail.com>
2017-05-10 15:58:07 -0400

    finish the classes for importer integration

---
+++ src/Importer/ImporterInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Omeka2Importer\Importer;
+
+interface ImporterInterface
+{
+    /**
+     * Return the updated $resourceJson
+     */
+    
+    public function import($itemData, $resourceJson);
+}
