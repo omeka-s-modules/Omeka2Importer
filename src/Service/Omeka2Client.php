aab667c5e28807b372e47efa3c257a03912fd643
John Flatness <john@zerocrates.org>
2021-02-09 16:59:22 -0500

    Remove unnecessary client cruft

--- src/Service/Omeka2Client.php
+++ src/Service/Omeka2Client.php
@@ -15,16 +15,6 @@ class Omeka2Client
     protected $httpClient;
 
     /**
-     * @var array
-     */
-    protected $methods = ['get', 'post', 'put', 'delete'];
-
-    /**
-     * @var array
-     */
-    protected $callbacks = [];
-
-    /**
      * @var string
      */
     protected $key;
@@ -67,45 +57,6 @@ class Omeka2Client
         $this->resource = $resource;
 
         return $this;
-    }
-
-    /**
-     * Method overloading.
-     *
-     * @param string $method
-     * @param array  $args
-     *
-     * @return mixed
-     */
-    public function __call($method, $args)
-    {
-        if (!in_array($method, $this->methods)) {
-            throw new \Exception('Invalid method.');
-        }
-        // Check for a callback.
-        if (array_key_exists($this->resource, $this->callbacks)
-            && array_key_exists($method, $this->callbacks[$this->resource])
-        ) {
-            $callback = $this->callbacks[$this->resource][$method];
-            // Prepend this Omeka client to the argument list.
-            array_unshift($args, $this);
-
-            return call_user_func_array($callback, $args);
-        }
-
-        return call_user_func_array([$this, $method], $args);
-    }
-
-    /**
-     * Set custom behavior for a resource/method.
-     *
-     * @param string   $resource
-     * @param string   $method
-     * @param \Closure $callback
-     */
-    public function setCallback($resource, $method, \Closure $callback)
-    {
-        $this->callbacks[$resource][$method] = $callback;
     }
 
     /**
@@ -165,7 +116,7 @@ class Omeka2Client
      *
      * @return Laminas\Http\Response
      */
-    protected function get($id = null, array $params = [])
+    public function get($id = null, array $params = [])
     {
         if (is_array($id)) {
             $params = $id;
@@ -174,57 +125,6 @@ class Omeka2Client
             $this->id = $id;
         }
         $client = $this->prepare(Request::METHOD_GET, $params);
-
-        return $client->send();
-    }
-
-    /**
-     * Make a POST request.
-     *
-     * @param string $data
-     * @param array  $params
-     *
-     * @return Laminas\Http\Response
-     */
-    protected function post($data, array $params = [])
-    {
-        $client = $this->prepare(Http\Request::METHOD_POST, $params)
-            ->setRawBody($data);
-
-        return $client->send();
-    }
-
-    /**
-     * Make a PUT request.
-     *
-     * @param int    $id
-     * @param string $data
-     * @param array  $params
-     *
-     * @return Laminas\Http\Response
-     */
-    protected function put($id, $data, array $params = [])
-    {
-        $this->id = $id;
-        $client = $this->prepare(Http\Request::METHOD_PUT, $params)
-            ->setRawBody($data);
-
-        return $client->send();
-    }
-
-    /**
-     * Make a DELETE request.
-     *
-     * @param int    $id
-     * @param string $data
-     * @param array  $params
-     *
-     * @return Laminas\Http\Response
-     */
-    protected function delete($id, array $params = [])
-    {
-        $this->id = $id;
-        $client = $this->prepare(Http\Request::METHOD_DELETE, $params);
 
         return $client->send();
     }
